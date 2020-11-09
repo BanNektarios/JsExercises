a97cb66b3d0227a7159d6f4bfab18a3104a9b270
Banousi Nektarios <63299482+BanNektarios@users.noreply.github.com>
2020-11-09 21:41:43 +0200

    Update objectTraining.js
    Added comments

--- objectTraining.js
+++ objectTraining.js
@@ -1,13 +1,15 @@
+// A class object
 class BankAccount {
+    // A constructor for our properties
     constructor(name, balance) {
         this.name = name;
         this.balance = balance;
-
+        // Our function to deposit money
         this.aDeposit = function (num) {
             this.num = num;
             return `${name} had ${balance}€ but he deposited ${num}€ and now has ${balance += num}€`
         }
-
+        // Our function to withdraw money
         this.aWithdraw = function (num) {
             this.num = num;
             return `${name} had ${balance}€ but he withdrew ${num}€ and now has ${balance -= num}€`;
@@ -15,6 +17,7 @@ class BankAccount {
     }
 }
 
+// The instance of the object we created
 const Person = new BankAccount("Nektarios", 12000);
 
 console.log(Person.aWithdraw(1000));
