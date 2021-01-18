bd04301ddd0276ef119745923e3872f975235867
BanNektarios <63299482+BanNektarios@users.noreply.github.com>
2021-01-18 20:08:52 +0200

    Playing with express

--- JS12-Exercises-main/software/nodejs_sample_app/index.js
+++ JS12-Exercises-main/software/nodejs_sample_app/index.js
@@ -1,40 +1,32 @@
-const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-const xhr = new XMLHttpRequest();
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
+const path = require('path')
 let url;
-getUrl()
-  .then(console.log("inside get " + url))
-  .then(console.log())
 
 
-async function getUrl() {
 
-  await app.get('/', (req, res) => {
-    console.log(req.url);
-    console.log(req.ip)
-    console.log(req.hostname)
-    async function reqListener() {
-      console.log(this.responseText);
-    }
-
-
-    xhr.addEventListener("load", reqListener);
-    xhr.open("GET", "::ffff:127.0.0.1:3000");
-    xhr.send()
-    app.path(req.url)
-    res.send(`<p>URL is: ${req.url}</p>`);
-    return url = req.url
-  })
-
-}
-
-
-app.listen(port, () => {
-  console.log("inside listen " + url);
-  console.log(`Example app listening at http://localhost:${port}`)
+app.get('/api/posts', (req,res) => {
+  res.json(posts)
 })
 
-console.log("outside listen " + url);
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+  // app.get('/', (req, res) => {
+  //   console.log(`Inside .get URL: ${req.url}`)
+  //   console.dir(req.url)
+  //   console.dir(req.query)
+  //   url = req.url
+  //   res.sendFile(path.join(__dirname, 'public', 'index.html'))
+  // })
+
+app.listen(port, () => {
+  console.log(".listen log: " + url);
+  console.log(`Example app listening at http://localhost:${port}`)})
+
+
+console.log("GLOBAL LOG: " + url)
+
 
