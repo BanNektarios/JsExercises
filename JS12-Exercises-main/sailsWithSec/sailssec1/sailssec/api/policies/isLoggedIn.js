0d07f9d89b7ec18edd6c3222d48742b4e4646c43
BanNektarios <63299482+BanNektarios@users.noreply.github.com>
2021-02-18 21:42:47 +0200

    asd

--- JS12-Exercises-main/sailsWithSec/sailssec1/sailssec/api/policies/isLoggedIn.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function (req, res, proceed) {
-    console.log("isLoggedIn")
-    if (req.me !== undefined) {
-        return proceed();
-    }
-    res.statusCode = 498
-    return res.view("pages/homepage", {data: "forbidden"})
-    // return res.forbidden();
-}
