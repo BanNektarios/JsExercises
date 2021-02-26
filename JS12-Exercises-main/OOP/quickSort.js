b7070345cc4e8e6d45b877f01466707abde21077
BanNektarios <63299482+BanNektarios@users.noreply.github.com>
2021-02-26 21:19:08 +0200

    a

--- JS12-Exercises-main/OOP/quickSort.js
+++ JS12-Exercises-main/OOP/quickSort.js
@@ -1,5 +1,3 @@
-const performance = require('performance');
-
 const unsortedArr = [31, 27, 28, 42, 13, 8, 11, 30, 17, 41, 15, 43, 1, 36, 9, 16, 20, 35, 48, 37, 7, 26, 34, 21, 22, 6, 29, 32, 49, 10, 12, 19, 24, 38, 5, 14, 44, 40, 3, 50, 46, 25, 18, 33, 47, 4, 45, 39, 23, 2];
 
 const sort = arr => {
@@ -13,6 +11,7 @@ const sort = arr => {
         if (arr[i] < pivot) left.push(arr[i]);
         else right.push(arr[i]);
     };
+    
     return [
         ...sort(left),
         pivot,
